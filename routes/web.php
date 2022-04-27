71266f173efaa9e98886af3881cbeff44911b857
pc <no-mail>
2022-04-27 03:51:31 +0300

    lesson1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+ */
+
+Route::get('/', function () {
+  return view('hello',);
+});
+
+Route::get('/info', function () {
+  return view('info', );
+});
+
+Route::get('/news', function () {
+  return view('news', );
 });
